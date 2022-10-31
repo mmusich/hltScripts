3d16a5688609741ec2256e29f69356d18dd8d3d6
Marino Missiroli <m.missiroli@cern.ch>
2022-10-31 10:19:00 +0100

    added hltTests/testCMSHLT2515.py

---
+++ hltTests/testCMSHLT2515.py
@@ -0,0 +1,26 @@
+from tsg import cms,process as tsg
+from hion import process as hin
+
+import os
+
+tsgPaths = sorted(list((foo[:foo.rfind('_v')]+'_v*' if '_v' in foo else foo) for foo in tsg.paths_() if not foo.startswith('Dataset_')))
+hinPaths = sorted(list((foo[:foo.rfind('_v')]+'_v*' if '_v' in foo else foo) for foo in hin.paths_() if not foo.startswith('Dataset_')))
+
+tsgFinalPaths = sorted(list(tsg.finalpaths_()))
+hinFinalPaths = sorted(list(hin.finalpaths_()))
+
+#hinPaths = [foo.replace('HIDataRawPrime','HITestRawPrime').replace('HIData','HITestRaw') for foo in hinPaths]
+#hinFinalPaths = [foo.replace('HIDataRawPrime','HITestRawPrime').replace('HIData','HITestRaw') for foo in hinFinalPaths]
+
+for [tsg_paths,hin_paths] in [
+  [tsgPaths, hinPaths],
+  [tsgFinalPaths, hinFinalPaths],
+]:
+  for hin_p in hin_paths:
+    if hin_p not in tsg_paths:
+      raise RuntimeError('missing: '+hin_p)
+
+  for tsg_p in tsg_paths:
+    if tsg_p not in hin_paths:
+      os.system('sed -i "/'+tsg_p+'/d" HLTrigger/Configuration/tables/HIon.txt')
+      os.system('sed -i "/'+tsg_p+'/d" HLTrigger/Configuration/tables/online_hion.txt')
