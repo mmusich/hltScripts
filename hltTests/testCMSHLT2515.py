7f33b4013d44eaa846cb9c4ea2c99f40a66acd34
Marino Missiroli <m.missiroli@cern.ch>
2022-11-02 17:29:04 +0100

    updated hltTests/testCMSHLT2515.py

--- hltTests/testCMSHLT2515.py
+++ hltTests/testCMSHLT2515.py
@@ -1,10 +1,14 @@
+#!/usr/bin/env python3
+
+# hltConfigFromDB --configName /dev/CMSSW_12_4_0/HIon > tsg.py
 from tsg import cms,process as tsg
+# hltConfigFromDB --configName /users/chenyi/PbPb2022/IonTest/HLT/V17 > hion.py
 from hion import process as hin
 
 import os
 
-tsgPaths = sorted(list((foo[:foo.rfind('_v')]+'_v*' if '_v' in foo else foo) for foo in tsg.paths_() if not foo.startswith('Dataset_')))
-hinPaths = sorted(list((foo[:foo.rfind('_v')]+'_v*' if '_v' in foo else foo) for foo in hin.paths_() if not foo.startswith('Dataset_')))
+tsgPaths = sorted(list((foo[:foo.rfind('_v')]+'_v\*' if '_v' in foo else foo) for foo in tsg.paths_() if not foo.startswith('Dataset_')))
+hinPaths = sorted(list((foo[:foo.rfind('_v')]+'_v\*' if '_v' in foo else foo) for foo in hin.paths_() if not foo.startswith('Dataset_')))
 
 tsgFinalPaths = sorted(list(tsg.finalpaths_()))
 hinFinalPaths = sorted(list(hin.finalpaths_()))
@@ -22,5 +26,5 @@ for [tsg_paths,hin_paths] in [
 
   for tsg_p in tsg_paths:
     if tsg_p not in hin_paths:
-      os.system('sed -i "/'+tsg_p+'/d" HLTrigger/Configuration/tables/HIon.txt')
-      os.system('sed -i "/'+tsg_p+'/d" HLTrigger/Configuration/tables/online_hion.txt')
+#      os.system('sed -i "s|'+tsg_p+'||g" HLTrigger/Configuration/tables/HIon.txt')
+      os.system('sed -i "s|'+tsg_p+'|#'+tsg_p+'|g" HLTrigger/Configuration/tables/online_hion.txt')
