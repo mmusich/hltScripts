4e98c03479c241c4e51441ac2f29ce175b84fccc
Marino Missiroli <m.missiroli@cern.ch>
2023-07-24 10:48:02 +0200

    updated storm/menuDev.sh

--- storm/menuDev.sh
+++ storm/menuDev.sh
@@ -13,13 +13,14 @@ stormCherryPick(){
 
 branch=$(git rev-parse --abbrev-ref HEAD)
 
+# git checkout -b HLTdevelopment130X_for133X && git commit -m 'HLT menu development for 13_0_X (branch: 13_3_X)' --allow-empty
 # git checkout -b HLTdevelopment130X_for132X && git commit -m 'HLT menu development for 13_0_X (branch: 13_2_X)' --allow-empty
 # git checkout -b HLTdevelopment130X_for131X && git commit -m 'HLT menu development for 13_0_X (branch: 13_1_X)' --allow-empty
-# git checkout -b HLTdevelopment130X && git commit -m 'HLT menu development for 13_0_X' --allow-empty
+# git checkout -b HLTdevelopment130X         && git commit -m 'HLT menu development for 13_0_X' --allow-empty
 
-if [ "${branch}" = HLTdevelopment130X_for132X ]; then
+if [ "${branch}" = HLTdevelopment130X_for133X ]; then
   sshkey_enable
-  cimsg='HLT menu development for 13_0_X (branch: 13_2_X)'
+  cimsg='HLT menu development for 13_0_X (branch: 13_3_X)'
   cd "${CMSSW_BASE}"/src/HLTrigger/Configuration/tables
   echo cms100kHz | ./makeSubTables
   cd "${CMSSW_BASE}"/src/HLTrigger/Configuration/test
@@ -27,6 +28,15 @@ if [ "${branch}" = HLTdevelopment130X_for132X ]; then
   git add -u :/
   git commit --amend --reset-author -m "${cimsg}"
   git push cms-tsg-storm "${branch}" -f
+  cd "${CMSSW_BASE}"/src
+
+elif [ "${branch}" = HLTdevelopment130X_for132X ]; then
+  [ $# -eq 1 ] || return 1
+  sshkey_enable
+  cimsg='HLT menu development for 13_0_X (branch: 13_2_X)'
+  hash=$1
+  cd "${CMSSW_BASE}"/src
+  stormCherryPick
   cd "${CMSSW_BASE}"/src
 
 elif [ "${branch}" = HLTdevelopment130X_for131X ]; then
