fd320fd9bab9dfabd5da97c9a6ee483edaeebd00
Marino Missiroli <m.missiroli@cern.ch>
2023-08-31 17:21:41 +0200

    updated storm/menuDev.sh

--- storm/menuDev.sh
+++ storm/menuDev.sh
@@ -13,14 +13,12 @@ stormCherryPick(){
 
 branch=$(git rev-parse --abbrev-ref HEAD)
 
-# git checkout -b HLTdevelopment130X_for133X && git commit -m 'HLT menu development for 13_0_X (branch: 13_3_X)' --allow-empty
-# git checkout -b HLTdevelopment130X_for132X && git commit -m 'HLT menu development for 13_0_X (branch: 13_2_X)' --allow-empty
-# git checkout -b HLTdevelopment130X_for131X && git commit -m 'HLT menu development for 13_0_X (branch: 13_1_X)' --allow-empty
-# git checkout -b HLTdevelopment130X         && git commit -m 'HLT menu development for 13_0_X' --allow-empty
+# git checkout -b HLTdevelopment132X_for133X && git commit -m 'HLT menu development for 13_2_X (branch: 13_3_X)' --allow-empty
+# git checkout -b HLTdevelopment132X         && git commit -m 'HLT menu development for 13_2_X' --allow-empty
 
-if [ "${branch}" = HLTdevelopment130X_for133X ]; then
+if [ "${branch}" = HLTdevelopment132X_for133X ]; then
   sshkey_enable
-  cimsg='HLT menu development for 13_0_X (branch: 13_3_X)'
+  cimsg='HLT menu development for 13_2_X (branch: 13_3_X)'
   cd "${CMSSW_BASE}"/src/HLTrigger/Configuration/tables
   echo cms100kHz | ./makeSubTables
   cd "${CMSSW_BASE}"/src/HLTrigger/Configuration/test
@@ -30,28 +28,10 @@ if [ "${branch}" = HLTdevelopment130X_for133X ]; then
   git push cms-tsg-storm "${branch}" -f
   cd "${CMSSW_BASE}"/src
 
-elif [ "${branch}" = HLTdevelopment130X_for132X ]; then
+elif [ "${branch}" = HLTdevelopment132X ]; then
   [ $# -eq 1 ] || return 1
   sshkey_enable
-  cimsg='HLT menu development for 13_0_X (branch: 13_2_X)'
-  hash=$1
-  cd "${CMSSW_BASE}"/src
-  stormCherryPick
-  cd "${CMSSW_BASE}"/src
-
-elif [ "${branch}" = HLTdevelopment130X_for131X ]; then
-  [ $# -eq 1 ] || return 1
-  sshkey_enable
-  cimsg='HLT menu development for 13_0_X (branch: 13_1_X)'
-  hash=$1
-  cd "${CMSSW_BASE}"/src
-  stormCherryPick
-  cd "${CMSSW_BASE}"/src
-
-elif [ "${branch}" = HLTdevelopment130X ]; then
-  [ $# -eq 1 ] || return 1
-  sshkey_enable
-  cimsg='HLT menu development for 13_0_X'
+  cimsg='HLT menu development for 13_2_X'
   hash=$1
   cd "${CMSSW_BASE}"/src
   stormCherryPick
