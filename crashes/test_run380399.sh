661a91b4e1e40a811b63b8e675df96168b7ade91
Marino Missiroli <m.missiroli@cern.ch>
2024-05-09 20:22:50 +0200

    added crashes/test_run380399.sh

---
+++ crashes/test_run380399.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -ex
+
+[ $# -ge 2 ] || exit 1
+
+hltGetConfiguration run:380399 \
+  --globaltag 140X_dataRun3_HLT_v3 \
+  --data \
+  --no-prescale \
+  --no-output \
+  --max-events -1 \
+  --input "${1}" \
+  > "${2}".py
+
+cat <<@EOF >> "${2}".py
+process.options.wantSummary = True
+
+process.options.numberOfThreads = 8
+process.options.numberOfStreams = 0
+@EOF
+
+cmsRun "${2}".py &> "${2}".log
