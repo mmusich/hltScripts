c8bd4d0df0192396b99aaad10b7641d9428ba21c
Marino Missiroli <m.missiroli@cern.ch>
2024-05-17 20:36:04 +0200

    updated fog/error_stream_root.sh

--- fog/error_stream_root.sh
+++ fog/error_stream_root.sh
@@ -1,12 +1,61 @@
 #!/bin/bash
 
-[ ! -z "${CMSSW_BASE}" ] || exit 1
+# help message
+showHelpMsg=false
+runNumber=-1
+
+helpMessage() {
+  cat <<@EOF
+Usage:
+  This script converts files in FRD format to ROOT formats (repacking).
+  It can be executed from any directory with access to CERN EOS.
+  Only input files in the EOS directory /eos/cms/store/group/tsg/FOG/error_stream/ are considered.
+  One number, N, is required as command-line argument.
+  This script will repack available error-stream files of run(s) *N*,
+  and place them in the EOS directories /eos/cms/store/group/tsg/FOG/error_stream_root/run*.
+
+  > ./error_stream_root.sh N
+
+Options:
+  -h, --help      Show this help message
+@EOF
+}
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -h|--help) showHelpMsg=true; shift;;
+    *) runNumber=$1; shift;;
+  esac
+done
+
+# print help message
+if [ ${showHelpMsg} == true ]; then
+  helpMessage
+  exit 0
+fi
+
+if ! [[ ${runNumber} =~ '^[0-9]+$' ]]; then
+  printf "%s\n" "ERROR -- Command-line argument is not a valid number: ${runNumber} (specify -h or --help for more info)."
+  exit 1 
+fi
+
+if [ -z "${CMSSW_BASE}" ]; then
+  printf "%s\n" "ERROR -- Environment variable CMSSW_BASE not initialised."
+  printf "%s\n" "         Please enable a recent CMSSW release before running this script."
+  exit 1
+fi
 
 cfgFile=${BASH_SOURCE[0]/.sh/_cfg.py}
-[ -f "${cfgFile}" ] || exit 1
 
-[ $# -eq 1 ] || exit 1
-fileNamesRaw=($(ls /eos/cms/store/group/tsg/FOG/error_stream/*"${1}"*/*raw 2> /dev/null))
+if [ ! -f "${cfgFile}" ]; then
+  printf "%s\n" "ERROR -- Target configuration file does not exist: ${cfgFile}"
+  printf "%s\n" "         This is unexpected. Please contact the FOG conveners."
+  exit 1
+fi
+
+fileNamesRaw=($(ls /eos/cms/store/group/tsg/FOG/error_stream/*"${runNumber}"*/*raw 2> /dev/null))
+
+rm -rf run000000
 
 for fileNameRaw in "${fileNamesRaw[@]}"; do
   fileNameRoot=${fileNameRaw/error_stream/error_stream_root}
@@ -16,3 +65,5 @@ for fileNameRaw in "${fileNamesRaw[@]}"; do
   cmsRun "${cfgFile}" "${fileNameRaw}" "${fileNameRoot}"
 done
 unset fileNameRaw fileNameRoot
+
+rm -rf run000000
