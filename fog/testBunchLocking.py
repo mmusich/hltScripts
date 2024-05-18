db2127d5fa5ae46cf51373c8e3693ae0404f1763
Marino Missiroli <m.missiroli@cern.ch>
2024-05-18 21:32:11 +0200

    updated fog/testBunchLocking.py

--- fog/testBunchLocking.py
+++ fog/testBunchLocking.py
@@ -1,17 +1,8 @@
 #!/usr/bin/env python3
 import sys
 
-nBunches = int(sys.argv[1])
-prescale = float(sys.argv[2])
-
 m_singlestep = 100
 trigger_counter = 0
-
-prescale_count = round(prescale * m_singlestep)
-
-nEvents = 10000 * nBunches
-
-l1aCounts = [0] * nBunches
 
 # https://github.com/cms-sw/cmssw/blob/d376eb350a3e9af9f0ce3618fbac6afc36ce56d8/L1Trigger/L1TGlobal/src/GlobalBoard.cc#L1241
 def accept():
@@ -22,8 +13,19 @@ def accept():
     trigger_counter -= prescale_count
     return True
 
-for event_i in range(nEvents):
-    l1aCounts[event_i % nBunches] += int(accept())
+if __name__ == '__main__':
 
-for bunch_i in range(nBunches):
-    print(f' {bunch_i: >5d} {l1aCounts[bunch_i]: >8d}')
+    nBunches = int(sys.argv[1])
+    l1aCounts = [0] * nBunches
+
+    prescale = float(sys.argv[2])
+    prescale_count = round(prescale * m_singlestep)
+
+    nEventsPerBunch = int(sys.argv[3]) if len(sys.argv) > 3 else 10000
+    nEvents = nBunches * nEventsPerBunch
+
+    for event_i in range(nEvents):
+        l1aCounts[event_i % nBunches] += int(accept())
+
+    for bunch_i in range(nBunches):
+        print(f' {bunch_i: >5d} {l1aCounts[bunch_i]: >8d}')
