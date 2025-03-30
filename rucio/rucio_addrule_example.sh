29b83778451517bee83216983683320f85a3666c
Marino Missiroli <m.missiroli@cern.ch>
2025-03-30 09:18:27 +0200

    updated rucio/rucio_addrule_example.sh

--- rucio/rucio_addrule_example.sh
+++ rucio/rucio_addrule_example.sh
@@ -11,22 +11,28 @@ datasets=(
 #  /EphemeralHLTPhysics5/Run2024I-v1/RAW#e4a0494d-c4ec-4562-b0cf-b824b373a25a
 #  /EphemeralHLTPhysics6/Run2024I-v1/RAW#bccb2fd8-8a2c-45f3-ae2a-d9fe92920052
 #  /EphemeralHLTPhysics7/Run2024I-v1/RAW#b538dc10-6715-4e81-92a0-9d1817e5b5c2
+
+/EphemeralHLTPhysics1/Run2024D-v1/RAW#7c0c2110-10a3-46f4-ad0d-3705acce70e0
+/EphemeralHLTPhysics1/Run2024D-v1/RAW#9e2b1a27-c4f9-433a-b4cd-f5e778a7c006
+
 )
 
 # rucio list-rules --account $RUCIO_ACCOUNT | grep Run2024 | grep Epheme
 
-export RUCIO_ACCOUNT="t2_ch_cern_local_users"
+#export RUCIO_ACCOUNT="t2_ch_cern_local_users"
 
 for dataset in "${datasets[@]}"; do
 
-#  rucio add-rule \
-#    cms:"${dataset}" \
-#    1 \
-#    'rse_type=DISK&cms_type=real\tier=3\tier=0' \
-#    --grouping 'ALL' \
-#    --lifetime 12960000 \
-#    --ask-approval \
-#    --activity "User AutoApprove" \
+  rucio add-rule \
+    cms:"${dataset}" \
+    1 \
+    'rse_type=DISK&cms_type=real\tier=3\tier=0' \
+    --grouping 'ALL' \
+    --lifetime 12960000 \
+    --ask-approval \
+    --activity "User AutoApprove" \
+    --comment "ECAL+HLT online operations"
+
 #    --comment "Trigger Studies Group (HLT)"
 
 #  rucio add-rule \
