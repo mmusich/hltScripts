ee57a47e214814acda855e19d3ecf156b778a7d9
Marino Missiroli <m.missiroli@cern.ch>
2025-06-11 08:31:09 +0200

    updated rucio/rucio_addrule_example.sh

--- rucio/rucio_addrule_example.sh
+++ rucio/rucio_addrule_example.sh
@@ -12,39 +12,39 @@ datasets=(
 #  /EphemeralHLTPhysics6/Run2024I-v1/RAW#bccb2fd8-8a2c-45f3-ae2a-d9fe92920052
 #  /EphemeralHLTPhysics7/Run2024I-v1/RAW#b538dc10-6715-4e81-92a0-9d1817e5b5c2
 
-#/EphemeralHLTPhysics1/Run2024D-v1/RAW#7c0c2110-10a3-46f4-ad0d-3705acce70e0
-#/EphemeralHLTPhysics1/Run2024D-v1/RAW#9e2b1a27-c4f9-433a-b4cd-f5e778a7c006
+/EphemeralHLTPhysics1/Run2024D-v1/RAW#7c0c2110-10a3-46f4-ad0d-3705acce70e0
+/EphemeralHLTPhysics1/Run2024D-v1/RAW#9e2b1a27-c4f9-433a-b4cd-f5e778a7c006
 
-/store/data/Run2024B/SpecialZeroBias0/RAW/v1/000/379/060/00000/5a0ee560-07e6-482e-bc67-89ca1ac8f73e.root
+#/store/data/Run2024B/SpecialZeroBias0/RAW/v1/000/379/060/00000/5a0ee560-07e6-482e-bc67-89ca1ac8f73e.root
 
 )
 
 # rucio list-rules --account $RUCIO_ACCOUNT | grep Run2024 | grep Epheme
 
-#export RUCIO_ACCOUNT="t2_ch_cern_local_users"
+export RUCIO_ACCOUNT="t2_ch_cern_local_users"
 
 for dataset in "${datasets[@]}"; do
-
-  rucio add-rule \
-    cms:"${dataset}" \
-    1 \
-    'rse_type=DISK&cms_type=real\tier=3\tier=0' \
-    --grouping 'ALL' \
-    --lifetime 2592000 \
-    --ask-approval \
-    --activity "User AutoApprove" \
-    --comment "Trigger Studies Group (HLT)"
-
-#    --comment "Trigger Studies Group (HLT)"
 
 #  rucio add-rule \
 #    cms:"${dataset}" \
 #    1 \
-#    T2_CH_CERN \
-#    --lifetime 12960000 \
+#    'rse_type=DISK&cms_type=real\tier=3\tier=0' \
+#    --grouping 'ALL' \
+#    --lifetime 2592000 \
 #    --ask-approval \
 #    --activity "User AutoApprove" \
 #    --comment "Trigger Studies Group (HLT)"
+
+#    --comment "Trigger Studies Group (HLT)"
+
+  rucio add-rule \
+    cms:"${dataset}" \
+    1 \
+    T2_CH_CERN \
+    --lifetime 15292800 \
+    --ask-approval \
+    --activity "User AutoApprove" \
+    --comment "ECAL Validation of Online Calibrations"
 
 done
 unset dataset
