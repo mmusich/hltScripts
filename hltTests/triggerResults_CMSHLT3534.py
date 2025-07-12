0dce28d77313d7cf5369a10b547840417c9d465f
Marino Missiroli <m.missiroli@cern.ch>
2025-07-12 23:14:41 +0200

    updated hltTests/triggerResults_CMSHLT3534.py

--- hltTests/triggerResults_CMSHLT3534.py
+++ hltTests/triggerResults_CMSHLT3534.py
@@ -12,26 +12,26 @@ if __name__ == '__main__':
     ### parameters
     ###
     minRunNumber = 0
-    maxRunNumber = 50
+    maxRunNumber = 99
     numEventsPerJob = -1
 
     numThreadsPerJobs = 32
     numStreamsPerJobs = 24
 
-    eosDirs = [f'/eos/cms/store/data/Run2024I/EphemeralHLTPhysics{foo}/RAW/v1/000/386/593/00000' for foo in range(7)]
+    eosDirs = [f'/eos/cms/store/data/Run2025C/HLTPhysics/RAW/v1/000/393/461/00000/']
 
-    hltMenu1 = '/users/missirol/test/dev/CMSSW_15_0_0/CMSHLT_3534/Test03/GRun/V1'
-    hltMenu2 = '/users/missirol/test/dev/CMSSW_15_0_0/CMSHLT_3534/Test03/GRun/V2'
+    hltMenu1 = '/users/missirol/test/dev/CMSSW_15_0_0/CMSHLT_3534/Test11/GRun/V1'
+    hltMenu2 = '/users/missirol/test/dev/CMSSW_15_0_0/CMSHLT_3534/Test11/GRun/V5'
 
-    hltLabel1 = sys.argv[1]+'_baseline'
-    hltLabel2 = sys.argv[1]+'_target'
+    hltLabel1 = sys.argv[1]+'_ref'
+    hltLabel2 = sys.argv[1]+'_tar'
 
     def hltGetCmd(hltMenu, hltLabel):
         return f"""
 https_proxy=http://cmsproxy.cms:3128/ \
 hltGetConfiguration {hltMenu} \
   --process HLTX \
-  --globaltag 150X_dataRun3_HLT_forTriggerStudies_v6 \
+  --globaltag 150X_dataRun3_HLT_v1 \
   --data \
   --prescale "2p0E34" \
   --output minimal \
@@ -60,83 +60,13 @@ process.options.accelerators = ['cpu']
     hltGetCmd1 = hltGetCmd(hltMenu1, hltLabel1)
     hltGetCmd2 = hltGetCmd(hltMenu2, hltLabel2)
 
-    customizeHLTfor2024L1TMenu = """
-def customizeHLTfor2024L1TMenu(process):
-    seed_replacements = {
+    config_ref = f"from {hltLabel1} import cms, process"
 
-        'L1_SingleMu5_BMTF' : 'L1_AlwaysTrue',
-        'L1_SingleMu13_SQ14_BMTF': 'L1_AlwaysTrue',
-
-        'L1_AXO_Medium' : 'L1_AXO_Nominal',
-        'L1_AXO_VVTight': 'L1_AlwaysTrue',
-        'L1_AXO_VVVTight': 'L1_AlwaysTrue',
-
-        'L1_CICADA_VVTight': 'L1_AlwaysTrue',
-        'L1_CICADA_VVVTight': 'L1_AlwaysTrue',
-        'L1_CICADA_VVVVTight': 'L1_AlwaysTrue',
-
-        'L1_DoubleTau_Iso34_Iso26_er2p1_Jet55_RmOvlp_dR0p5': 'L1_DoubleIsoTau26er2p1_Jet55_RmOvlp_dR0p5 OR L1_DoubleIsoTau26er2p1_Jet70_RmOvlp_dR0p5',
-        'L1_DoubleTau_Iso38_Iso26_er2p1_Jet55_RmOvlp_dR0p5': 'L1_DoubleIsoTau26er2p1_Jet55_RmOvlp_dR0p5 OR L1_DoubleIsoTau26er2p1_Jet70_RmOvlp_dR0p5',
-        'L1_DoubleTau_Iso40_Iso26_er2p1_Jet55_RmOvlp_dR0p5': 'L1_DoubleIsoTau26er2p1_Jet55_RmOvlp_dR0p5 OR L1_DoubleIsoTau26er2p1_Jet70_RmOvlp_dR0p5',
-
-        'L1_DoubleEG15_11_er1p2_dR_Max0p6': 'L1_DoubleEG11_er1p2_dR_Max0p6',
-        'L1_DoubleEG16_11_er1p2_dR_Max0p6': 'L1_DoubleEG11_er1p2_dR_Max0p6',
-        'L1_DoubleEG17_11_er1p2_dR_Max0p6': 'L1_DoubleEG11_er1p2_dR_Max0p6',
-
-        'L1_DoubleEG15_er1p5_dEta_Max1p5': 'L1_AlwaysTrue',
-        'L1_DoubleEG16_er1p5_dEta_Max1p5': 'L1_AlwaysTrue',
-        'L1_DoubleEG17_er1p5_dEta_Max1p5': 'L1_AlwaysTrue',
-
-        'L1_DoubleJet_110_35_DoubleJet35_Mass_Min1000': 'L1_AlwaysTrue',
-        'L1_DoubleJet_110_35_DoubleJet35_Mass_Min1100': 'L1_AlwaysTrue',
-        'L1_DoubleJet_110_35_DoubleJet35_Mass_Min1200': 'L1_AlwaysTrue',
-        'L1_DoubleJet45_Mass_Min700_IsoTau45er2p1_RmOvlp_dR0p5': 'L1_AlwaysTrue',
-        'L1_DoubleJet45_Mass_Min800_IsoTau45er2p1_RmOvlp_dR0p5': 'L1_AlwaysTrue',
-        'L1_DoubleJet_65_35_DoubleJet35_Mass_Min750_DoubleJetCentral50': 'L1_AlwaysTrue',
-        'L1_DoubleJet_65_35_DoubleJet35_Mass_Min850_DoubleJetCentral50': 'L1_AlwaysTrue',
-        'L1_DoubleJet_65_35_DoubleJet35_Mass_Min950_DoubleJetCentral50': 'L1_AlwaysTrue',
-        'L1_DoubleJet45_Mass_Min700_LooseIsoEG20er2p1_RmOvlp_dR0p2': 'L1_AlwaysTrue',
-        'L1_DoubleJet45_Mass_Min800_LooseIsoEG20er2p1_RmOvlp_dR0p2': 'L1_AlwaysTrue',
-        'L1_DoubleJet_85_35_DoubleJet35_Mass_Min700_Mu3OQ': 'L1_AlwaysTrue',
-        'L1_DoubleJet_85_35_DoubleJet35_Mass_Min800_Mu3OQ': 'L1_AlwaysTrue',
-        'L1_DoubleJet_85_35_DoubleJet35_Mass_Min900_Mu3OQ': 'L1_AlwaysTrue',
-        'L1_DoubleJet_70_35_DoubleJet35_Mass_Min600_ETMHF65': 'L1_AlwaysTrue',
-        'L1_DoubleJet_70_35_DoubleJet35_Mass_Min700_ETMHF65': 'L1_AlwaysTrue',
-        'L1_DoubleJet_70_35_DoubleJet35_Mass_Min800_ETMHF65': 'L1_AlwaysTrue',
-    }
-
-    for module in filters_by_type(process, 'HLTL1TSeed'):
-        l1Seed = module.L1SeedsLogicalExpression.value()
-        if any(old_seed in l1Seed for old_seed in seed_replacements):
-            for old_seed, new_seed in seed_replacements.items():
-                l1Seed = l1Seed.replace(old_seed, new_seed)
-            module.L1SeedsLogicalExpression = cms.string(l1Seed)
-
-    return process
-"""
-
-    config_customisation = f"""
-from HLTrigger.Configuration.common import *
-
-process.GlobalTag.globaltag = '150X_dataRun3_HLT_forTriggerStudies_v6'
-
-{customizeHLTfor2024L1TMenu}
-
-process = customizeHLTfor2024L1TMenu(process)
-
-for prod in producers_by_type(process, 'CaloTowersCreator'):
-    prod.EcalRecHitThresh = True
-"""
-
-    config_baseline = f"from {hltLabel1} import cms, process"
-    config_baseline += config_customisation
-
-    config_target = f"from {hltLabel2} import cms, process"
-    config_target += config_customisation
+    config_tar = f"from {hltLabel2} import cms, process"
 
     hltCfgTypes = {
-        f'{hltLabel1}_GTv6': config_baseline,
-        f'{hltLabel2}_GTv6': config_target,
+        f'{hltLabel1}_V1': config_ref,
+        f'{hltLabel2}_V5': config_tar,
     }
 
     print(f'Creating list of EDM input files on EOS ...')
