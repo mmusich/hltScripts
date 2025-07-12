5a2f136f2ac77ec8c5aa60c69df21acae22d223d
Marino Missiroli <m.missiroli@cern.ch>
2025-07-12 17:28:21 +0200

    updated rucio/rucio_addrule_example.sh

--- rucio/rucio_addrule_example.sh
+++ rucio/rucio_addrule_example.sh
@@ -12,10 +12,12 @@ datasets=(
 #  /EphemeralHLTPhysics6/Run2024I-v1/RAW#bccb2fd8-8a2c-45f3-ae2a-d9fe92920052
 #  /EphemeralHLTPhysics7/Run2024I-v1/RAW#b538dc10-6715-4e81-92a0-9d1817e5b5c2
 
-/EphemeralHLTPhysics1/Run2024D-v1/RAW#7c0c2110-10a3-46f4-ad0d-3705acce70e0
-/EphemeralHLTPhysics1/Run2024D-v1/RAW#9e2b1a27-c4f9-433a-b4cd-f5e778a7c006
+#/EphemeralHLTPhysics1/Run2024D-v1/RAW#7c0c2110-10a3-46f4-ad0d-3705acce70e0
+#/EphemeralHLTPhysics1/Run2024D-v1/RAW#9e2b1a27-c4f9-433a-b4cd-f5e778a7c006
 
 #/store/data/Run2024B/SpecialZeroBias0/RAW/v1/000/379/060/00000/5a0ee560-07e6-482e-bc67-89ca1ac8f73e.root
+
+/store/data/Run2024D/AlCaLumiPixelsCountsPrompt1/RAW/v1/000/380/895/00000/c27ff3c5-8ad9-4cf2-b359-2abfd7bc1b1d.root
 
 )
 
@@ -41,10 +43,10 @@ for dataset in "${datasets[@]}"; do
     cms:"${dataset}" \
     1 \
     T2_CH_CERN \
-    --lifetime 15292800 \
+    --lifetime 400000 \
     --ask-approval \
     --activity "User AutoApprove" \
-    --comment "ECAL Validation of Online Calibrations"
+    --comment "Trigger Studies Group (TSG, HLT)"
 
 done
 unset dataset
