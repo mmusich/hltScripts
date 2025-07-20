dca75fec832e151e52ef704b7ac070476a8936ab
Marino Missiroli <m.missiroli@cern.ch>
2025-07-20 18:02:54 +0200

    updated hltTests/testCMSHLT3534_checkPaths.py

--- hltTests/testCMSHLT3534_checkPaths.py
+++ hltTests/testCMSHLT3534_checkPaths.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 
-os.system('hltConfigFromDB --configName /dev/CMSSW_15_0_0/GRun/V57 > GRun.py')
+os.system('hltConfigFromDB --configName /dev/CMSSW_15_0_0/GRun/V98 > GRun.py')
 from GRun import cms,process
 
 ret = []
@@ -9,32 +9,46 @@ ret = []
 for pathName in process.paths_():
     path = getattr(process, pathName)
 
-    flag1 = False
-    flag2 = False
-    flag3 = False
+    hasTracks = path.contains(process.hltMergedTracks)
 
-    keepPath = False
+    hasSeq = False
+    hasSeq |= path.contains(process.HLTTrackReconstructionForPF)
+    hasSeq |= path.contains(process.HLTTrackReconstructionForPFDispl)
+    hasSeq |= path.contains(process.HLTTrackReconstructionForPFNoMu)
+    hasSeq |= path.contains(process.HLTTrackReconstructionForPFSerialSync)
+    hasSeq |= path.contains(process.HLTTrackerMuonSequenceLowPt)
 
-    for (type_i, label_i) in path.expandAndClone().directDependencies():
-        if type_i != 'modules':
-            continue
+    if hasTracks and not hasSeq:
+        print(pathName)
 
-        if label_i == 'hltSiStripRawToClustersFacility':
-            flag1 = True
-
-        if label_i == 'hltIter0PFlowTrackSelectionHighPurity':
-            flag2 = True
-
-        if label_i == 'hltParticleFlow':
-            flag3 = True
-
-    if flag1 and flag2 and (not flag3):
-        keepPath = True
-
-    if keepPath:
-        ret += [pathName]
-
-ret = sorted(list(set(ret)))
-
-for foo in ret:
-    print(foo)
+#    flag1 = False
+#    flag2 = False
+#    flag3 = False
+#
+#    keepPath = False
+#
+#    for (type_i, label_i) in path.expandAndClone().directDependencies():
+#        if type_i != 'sequences':
+#            continue
+#        print(type_i)
+#        continue
+#
+#        if label_i == 'hltSiStripRawToClustersFacility':
+#            flag1 = True
+#
+#        if label_i == 'hltIter0PFlowTrackSelectionHighPurity':
+#            flag2 = True
+#
+#        if label_i == 'hltParticleFlow':
+#            flag3 = True
+#
+#    if flag1 and flag2 and (not flag3):
+#        keepPath = True
+#
+#    if keepPath:
+#        ret += [pathName]
+#
+#ret = sorted(list(set(ret)))
+#
+#for foo in ret:
+#    print(foo)
