95b5812ae8ffe6786da51a15d671c1c0d5817973
Marino Missiroli <m.missiroli@cern.ch>
2025-08-04 16:12:23 +0200

    updated hltTests/testCMSHLT3534_prompt.sh

--- hltTests/testCMSHLT3534_prompt.sh
+++ hltTests/testCMSHLT3534_prompt.sh
@@ -8,10 +8,11 @@ hltLabel="${jobLabel}"_hlt
 promptLabel="${jobLabel}"_prompt
 
 inputFile=root://eoscms.cern.ch//eos/cms/store/data/Run2025D/EGamma0/RAW/v1/000/394/959/00000/906d7971-ad62-4927-ad21-8c5691d5dfda.root
-maxEvents=10
+maxEvents=300
 
 https_proxy=http://cmsproxy.cms:3128/ \
 hltGetConfiguration "${hltMenu}" \
+ --process HLTX \
  --globaltag 150X_dataRun3_HLT_v1 \
  --data \
  --no-prescale \
@@ -30,6 +31,10 @@ for foo in streamPaths:
     process.__delattr__(foo)
 
 process.hltOutputHLTMonitor.fileName = "${hltLabel}.root"
+
+process.hltOutputHLTMonitor.outputCommands += [
+#    "drop *_hltSiStripRawToClustersFacilityForMkFit_*_*",
+]
 @EOF
 
 cmsRun "${hltLabel}".py &> "${hltLabel}".log
@@ -39,10 +44,27 @@ python3 Configuration/DataProcessing/test/RunPromptReco.py \
   --global-tag 150X_dataRun3_Express_v2 \
   --lfn=file:"${hltLabel}".root \
   --reco \
-  --alcarecos=TkAlHLTTracks+TkAlHLTTracksZMuMu+PromptCalibProdSiPixelAliHLTHGC \
   --dqm \
   --dqmSeq=@HLTMon
 
+#  --alcarecos=TkAlHLTTracks+TkAlHLTTracksZMuMu+PromptCalibProdSiPixelAliHLTHGC \
+
+cat <<@EOF >> RunPromptRecoCfg.py
+
+#process.pathALCARECOTkAlHLTTracks = cms.EndPath(process.seqALCARECOTkAlHLTTracks+process.ALCARECOTkAlHLTTracksDQM)
+#process.pathALCARECOTkAlHLTTracksZMuMu = cms.EndPath(process.seqALCARECOTkAlHLTTracksZMuMu+process.ALCARECOTkAlHLTTracksZMuMuDQM)
+#process.pathALCARECOPromptCalibProdSiPixelAliHLTHGMinBias = cms.EndPath(process.seqALCARECOPromptCalibProdSiPixelAliHLTHG)
+#process.pathALCARECOPromptCalibProdSiPixelAliHLTHGDiMu = cms.EndPath(process.seqALCARECOPromptCalibProdSiPixelAliHLTHGDiMu)
+
+process.options.wantSummary = True
+
+alcaPaths = [foo for foo in process.paths_() if foo.startswith('pathALCARECO')]
+for foo in alcaPaths:
+    process.__delattr__(foo)
+@EOF
+
 edmConfigDump RunPromptRecoCfg.py > "${promptLabel}".py
+
+sed -i 's|"TriggerResults","","HLT"|"TriggerResults","","HLTX"|g' "${promptLabel}".py
 
 cmsRun "${promptLabel}".py &> "${promptLabel}".log
