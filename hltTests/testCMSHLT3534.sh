23a31efc5ba70edfe5c4d0ee574db844dfed9fdc
Marino Missiroli <m.missiroli@cern.ch>
2025-08-08 07:53:47 +0200

    updated hltTests/testCMSHLT3534.sh (more events)

--- hltTests/testCMSHLT3534.sh
+++ hltTests/testCMSHLT3534.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
-maxEvents=1
-skipEvents=680
+maxEvents=-1
+skipEvents=0
 
 run(){
 
@@ -12,7 +12,12 @@ run(){
    --no-prescale \
    --output minimal \
    --max-events "${maxEvents}" \
-   --input /store/mc/Run3Winter25Digi/TT_TuneCP5_13p6TeV_powheg-pythia8/GEN-SIM-RAW/142X_mcRun3_2025_realistic_v7-v2/130000/f180085b-343d-4d33-9bda-e859a0c8d4dd.root \
+   --input \
+/store/mc/Run3Winter25Digi/TT_TuneCP5_13p6TeV_powheg-pythia8/GEN-SIM-RAW/142X_mcRun3_2025_realistic_v7-v2/130000/f180085b-343d-4d33-9bda-e859a0c8d4dd.root,\
+/store/mc/Run3Winter25Digi/TT_TuneCP5_13p6TeV_powheg-pythia8/GEN-SIM-RAW/142X_mcRun3_2025_realistic_v7-v2/130000/fee54b2e-e89c-4321-b2b3-e435a2c4ba79.root,\
+/store/mc/Run3Winter25Digi/TT_TuneCP5_13p6TeV_powheg-pythia8/GEN-SIM-RAW/142X_mcRun3_2025_realistic_v7-v2/130000/ffa44d93-e86c-46ba-94d7-2b60b2b05b2f.root,\
+/store/mc/Run3Winter25Digi/TT_TuneCP5_13p6TeV_powheg-pythia8/GEN-SIM-RAW/142X_mcRun3_2025_realistic_v7-v2/130000/ffa7eed1-0f84-4624-b881-0e6c904e487f.root,\
+/store/mc/Run3Winter25Digi/TT_TuneCP5_13p6TeV_powheg-pythia8/GEN-SIM-RAW/142X_mcRun3_2025_realistic_v7-v2/130000/ffee558e-eea1-4898-9839-1e7ffe6b0fa1.root \
    --eras Run3_2025 --l1-emulator uGT --l1 L1Menu_Collisions2025_v1_3_0_xml \
    --paths "AlCa_PFJet40_*","HLT_DoubleMu3_TkMu_DsTau3Mu_v*","HLT_DisplacedMu24_MediumChargedIsoDisplacedPFTauHPS24_v*" \
    > "${1}".py
@@ -49,5 +54,5 @@ process.hltOutputMinimal.outputCommands += [
   cmsRun "${1}".py &> "${1}".log
 }
 
-run testCMSHLT3534_hlt1 /dev/CMSSW_15_0_0/GRun/V103
-run testCMSHLT3534_hlt2 /users/missirol/test/dev/CMSSW_15_0_0/CMSHLT_3534/Test21/GRun/V3
+run testCMSHLT3534_hlt1 /dev/CMSSW_15_0_0/GRun/V110
+run testCMSHLT3534_hlt2 /users/missirol/test/dev/CMSSW_15_0_0/CMSHLT_3534/Test31/GRun/V3
