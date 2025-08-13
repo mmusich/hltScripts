384a50696df44c143cfd968c73ef32c3d5c7ff77
Marino Missiroli <m.missiroli@cern.ch>
2025-08-13 11:39:20 +0200

    updated storm/findUnusedPathsInCombinedTable.py

--- storm/findUnusedPathsInCombinedTable.py
+++ storm/findUnusedPathsInCombinedTable.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 import os
 
-os.system('hltConfigFromDB --configName /dev/CMSSW_15_0_0/HLT > HLT.py')
+for hltCmd in [
+    'hltConfigFromDB --configName /dev/CMSSW_15_0_0/HLT > HLT.py',
+    'hltConfigFromDB --configName /dev/CMSSW_15_0_0/GRun > GRun.py',
+    'hltConfigFromDB --configName /dev/CMSSW_15_0_0/HIon > HIon.py',
+    'hltConfigFromDB --configName /dev/CMSSW_15_0_0/PRef > PRef.py',
+    'hltConfigFromDB --configName /dev/CMSSW_15_0_0/PIon > PIon.py',
+    'hltConfigFromDB --configName /dev/CMSSW_15_0_0/Special > Spec.py',
+]:
+    print(hltCmd)
+    os.system(f'{hltCmd}')
+
 from HLT import cms,process as hlt
-
-os.system('hltConfigFromDB --configName /dev/CMSSW_15_0_0/GRun > GRun.py')
 from GRun import process as grun
-
-os.system('hltConfigFromDB --configName /dev/CMSSW_15_0_0/HIon > HIon.py')
 from HIon import process as hion
-
-os.system('hltConfigFromDB --configName /dev/CMSSW_15_0_0/PRef > PRef.py')
 from PRef import process as pref
-
-os.system('hltConfigFromDB --configName /dev/CMSSW_15_0_0/PIon > PIon.py')
 from PIon import process as pion
-
-os.system('hltConfigFromDB --configName /dev/CMSSW_15_0_0/Special > Spec.py')
 from Spec import process as spec
 
 paths = []
@@ -24,6 +24,7 @@ for pathlist in [hlt.paths_(), hlt.endpaths_()]:
   for foo in pathlist:
     paths.append(foo)
 paths = sorted(list(set(paths)))
+
 for foo in paths:
   if foo in grun.paths_(): continue
   if foo in hion.paths_(): continue
