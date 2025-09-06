61ac6a23f32677bf98c5126d6d04c988fd14ef3e
Marino Missiroli <m.missiroli@cern.ch>
2025-09-06 11:24:24 +0200

    updated hltTests/test_cmssw43214.sh

--- hltTests/test_cmssw43214.sh
+++ hltTests/test_cmssw43214.sh
@@ -1,47 +1,46 @@
-#!/bin/bash
+#!/bin/bash -ex
 
-execmd="hltGetConfiguration /dev/CMSSW_14_0_0/HIon --no-prescale --no-output --max-events 100"
+inputFileData=file:/eos/cms/store/user/cmsbuild/store/hidata/HIRun2024B/HIForward0/RAW/v1/000/388/784/00000/a277c6d8-c445-4d2b-a45c-0e74e4ed8ce8.root
+
+execmd="hltGetConfiguration /dev/CMSSW_15_0_0/HIon/V114"
+execmd+=" --no-prescale --no-output"
 execmd+=" --paths HLTriggerFirstPath,HLTriggerFinalPath,HLTAnalyzerEndpath"
+execmd+=" --customise HLTrigger/Configuration/CustomConfigs.customiseHLTforHIonRepackedRAW"
+execmd+=" --globaltag 150X_dataRun3_HLT_v1 --data"
+execmd+=" --input ${inputFileData}"
+execmd+=" --max-events 10000"
+
+run(){
+  jobLabel="${1}"
+  date && echo "${jobLabel}"
+  ${execmd} ${@:2} > hltData_"${jobLabel}".py
+  cmsRun hltData_"${jobLabel}".py &> hltData_"${jobLabel}".log
+  grep L1_ZDC hltData_"${jobLabel}".log > hltData_"${jobLabel}"_L1ZDC.txt
+  date
+}
+
+run ref
+run L1TEmulGT   --l1-emulator uGT
+run L1TEmulFull --l1-emulator Full
+
+diff hltData_ref_L1ZDC.txt hltData_L1TEmulFull_L1ZDC.txt
 
 #${execmd} \
-#  --customise HLTrigger/Configuration/CustomConfigs.customiseHLTforHIonRepackedRAWPrime \
-#  --input file:/eos/cms/store/user/cmsbuild/store/hidata/HIRun2023A/HIPhysicsRawPrime0/RAW/v1/000/375/491/00000/de963321-c0a0-49fb-b771-1a312a69db03.root \
-#  --globaltag 140X_dataRun3_HLT_v3 \
-#  --data \
-#  > hltData_ref.py && cmsRun hltData_ref.py >& hltData_ref.log
+#  --input /store/relval/CMSSW_14_0_0/RelValPyquen_DiJet_pt80to120_5362GeV_2023/GEN-SIM-DIGI-RAW-HLTDEBUG/PU_140X_mcRun3_2023_realistic_HI_v3_STD_HIN_PU-v1/2580000/8dda8ff3-5f50-4698-8834-24f2a31a8bf3.root \
+#  --globaltag 140X_mcRun3_2023_realistic_HI_v3 \
+#  --mc \
+#  > hltMC_ref.py && cmsRun hltMC_ref.py &> hltMC_ref.log
 #
 #${execmd} \
-#  --customise HLTrigger/Configuration/CustomConfigs.customiseL1THLTforHIonRepackedRAWPrime \
-#  --input file:/eos/cms/store/user/cmsbuild/store/hidata/HIRun2023A/HIPhysicsRawPrime0/RAW/v1/000/375/491/00000/de963321-c0a0-49fb-b771-1a312a69db03.root \
-#  --globaltag 140X_dataRun3_HLT_v3 \
-#  --data \
+#  --input /store/relval/CMSSW_14_0_0/RelValPyquen_DiJet_pt80to120_5362GeV_2023/GEN-SIM-DIGI-RAW-HLTDEBUG/PU_140X_mcRun3_2023_realistic_HI_v3_STD_HIN_PU-v1/2580000/8dda8ff3-5f50-4698-8834-24f2a31a8bf3.root \
+#  --globaltag 140X_mcRun3_2023_realistic_HI_v3 \
+#  --mc \
 #  --eras Run3 --l1-emulator uGT \
-#  > hltData_L1uGT.py && cmsRun hltData_L1uGT.py >& hltData_L1uGT.log
+#  > hltMC_L1uGT.py && cmsRun hltMC_L1uGT.py &> hltMC_L1uGT.log
 #
 #${execmd} \
-#  --customise HLTrigger/Configuration/CustomConfigs.customiseL1THLTforHIonRepackedRAWPrime \
-#  --input file:/eos/cms/store/user/cmsbuild/store/hidata/HIRun2023A/HIPhysicsRawPrime0/RAW/v1/000/375/491/00000/de963321-c0a0-49fb-b771-1a312a69db03.root \
-#  --globaltag 140X_dataRun3_HLT_v3 \
-#  --data \
-#  --eras Run3 --l1-emulator Full \
-#  > hltData_L1Full.py && cmsRun hltData_L1Full.py >& hltData_L1Full.log
-
-${execmd} \
-  --input /store/relval/CMSSW_14_0_0/RelValPyquen_DiJet_pt80to120_5362GeV_2023/GEN-SIM-DIGI-RAW-HLTDEBUG/PU_140X_mcRun3_2023_realistic_HI_v3_STD_HIN_PU-v1/2580000/8dda8ff3-5f50-4698-8834-24f2a31a8bf3.root \
-  --globaltag 140X_mcRun3_2023_realistic_HI_v3 \
-  --mc \
-  > hltMC_ref.py && cmsRun hltMC_ref.py &> hltMC_ref.log
-
-${execmd} \
-  --input /store/relval/CMSSW_14_0_0/RelValPyquen_DiJet_pt80to120_5362GeV_2023/GEN-SIM-DIGI-RAW-HLTDEBUG/PU_140X_mcRun3_2023_realistic_HI_v3_STD_HIN_PU-v1/2580000/8dda8ff3-5f50-4698-8834-24f2a31a8bf3.root \
-  --globaltag 140X_mcRun3_2023_realistic_HI_v3 \
-  --mc \
-  --eras Run3 --l1-emulator uGT \
-  > hltMC_L1uGT.py && cmsRun hltMC_L1uGT.py &> hltMC_L1uGT.log
-
-${execmd} \
-  --input /store/relval/CMSSW_14_0_0/RelValPyquen_DiJet_pt80to120_5362GeV_2023/GEN-SIM-DIGI-RAW-HLTDEBUG/PU_140X_mcRun3_2023_realistic_HI_v3_STD_HIN_PU-v1/2580000/8dda8ff3-5f50-4698-8834-24f2a31a8bf3.root \
-  --globaltag 140X_mcRun3_2023_realistic_HI_v3 \
-  --mc \
-  --eras Run3 --l1-emulator FullMC \
-  > hltMC_L1FullMC.py && cmsRun hltMC_L1FullMC.py &> hltMC_L1FullMC.log
+#  --input /store/relval/CMSSW_14_0_0/RelValPyquen_DiJet_pt80to120_5362GeV_2023/GEN-SIM-DIGI-RAW-HLTDEBUG/PU_140X_mcRun3_2023_realistic_HI_v3_STD_HIN_PU-v1/2580000/8dda8ff3-5f50-4698-8834-24f2a31a8bf3.root \
+#  --globaltag 140X_mcRun3_2023_realistic_HI_v3 \
+#  --mc \
+#  --eras Run3 --l1-emulator FullMC \
+#  > hltMC_L1FullMC.py && cmsRun hltMC_L1FullMC.py &> hltMC_L1FullMC.log
